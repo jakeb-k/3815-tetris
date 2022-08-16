6c5a2865fde777c715d4056b2ef4a31c634c1db7
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2022-08-17 08:57:23 +1000

    added comments

--- sketch.js
+++ sketch.js
@@ -7,11 +7,16 @@ let s;
 let boxX;
 let boxY; 
 let collide; 
+let colCount = 0; 
+let gameRun = true; 
 
 
 function preload(){
   rectImage = loadImage('rectangle.png'); 
 }
+
+
+
 
 function setup(){
   blocks = new Group();
@@ -27,12 +32,15 @@ function initBlock(){
 
   // need to create object array that defines different size blocks
   // need to randomly call them when creating the sprite
-
+   
   angleMode(RADIANS);
+  if(colCount < 2) {
   s = createSprite(width/2 +10, height%20, 48, 148);
   dBlocks.add(s)  
   dBlocks.direction = HALF_PI; 
-  dBlocks.speed = 4;  
+  dBlocks.speed = 5;  
+  }
+  
   
 }
 
@@ -41,27 +49,43 @@ function stopBlock(sprite){
   // need to add control that only inits once
   // even if has collided with multiple blocks
 
+  // have added the control, but colCount continues to add
+  // on single collision with another block
+  // and will inevitably stop dropping
+  
+  colCount +=1; 
   sprite.speed = 0;
   sprite.direction = 0;
   sprite.rotationLocked = true;  
   sprite.immovable = true;  
   dBlocks.remove(sprite); 
   blocks.add(sprite); 
-  initBlock();    
+  initBlock();   
+}
+function colliderBlock(sprite){
+  colCount = 0;
+  sprite.speed = 0;
+  sprite.direction = 0;
+  sprite.rotationLocked = true;  
+  sprite.immovable = true;  
+  dBlocks.remove(sprite); 
+  blocks.add(sprite); 
+  initBlock();   
+
 }
 
 
 function draw(){
   background(125,125,125);
   gridDraw();
-  
-  dBlocks.collide(collideSprite, stopBlock);  
+  dBlocks.collide(collideSprite, colliderBlock);  
   dBlocks.collide(blocks, stopBlock);  
 }
 
 
 
-
+//add constraint to check if s.x % 50 is true and to move to
+// the closest multiple of 50 if not 
 function keyReleased() {
    if (keyCode === UP_ARROW) {    
     s.rotation += HALF_PI; 
@@ -76,12 +100,16 @@ function keyReleased() {
   }
 }
 
+//add grid constraint function
+
 
 function gridDraw() {
 	for(let i=35; i<=500; i+=50) {
 		for(let h=35; h<=1000;h+=50) {
-			noFill();   
-			rect(i,h,50,50); 
+			noFill(); 
+      strokeWeight(5);   
+			rect(i,h,51,51); 
+      strokeWeight(2); 
 		}
 	}
 }

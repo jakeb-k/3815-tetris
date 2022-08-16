b404effb1ae915b8269bf20fa9ebcb9fefd7fe48
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2022-08-17 08:53:37 +1000

    2nd commit have added collision and dropping

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 
-
+// need to add startup page, high score page and settings page
+// need to display all relevant information on said pages  
 
 let s;
 
@@ -7,17 +8,46 @@ let boxX;
 let boxY; 
 let collide; 
 
+
 function preload(){
   rectImage = loadImage('rectangle.png'); 
 }
 
 function setup(){
+  blocks = new Group();
+  dBlocks = new Group();
   createCanvas(600,1100);
-  s = createSprite(width/2 +10, height%20, 50, 150);
-  s.direction = 90;
-  s.speed = 2; 
-  
+  initBlock(); 
+  collideSprite = createSprite(width/2, 1061, width, 50, physics='static'); 
+  collideSprite.shapeColor = (125,125,125); 
   angleMode(RADIANS); 
+}
+
+function initBlock(){
+
+  // need to create object array that defines different size blocks
+  // need to randomly call them when creating the sprite
+
+  angleMode(RADIANS);
+  s = createSprite(width/2 +10, height%20, 48, 148);
+  dBlocks.add(s)  
+  dBlocks.direction = HALF_PI; 
+  dBlocks.speed = 4;  
+  
+}
+
+function stopBlock(sprite){
+
+  // need to add control that only inits once
+  // even if has collided with multiple blocks
+
+  sprite.speed = 0;
+  sprite.direction = 0;
+  sprite.rotationLocked = true;  
+  sprite.immovable = true;  
+  dBlocks.remove(sprite); 
+  blocks.add(sprite); 
+  initBlock();    
 }
 
 
@@ -25,12 +55,12 @@ function draw(){
   background(125,125,125);
   gridDraw();
   
-  // will need to set bool that changes the collision part
-  // depending on current rotation
-  if(s.y > 960){
-    s.speed = 0; 
-  }
+  dBlocks.collide(collideSprite, stopBlock);  
+  dBlocks.collide(blocks, stopBlock);  
 }
+
+
+
 
 function keyReleased() {
    if (keyCode === UP_ARROW) {    
@@ -38,11 +68,14 @@ function keyReleased() {
   }
   if(keyCode == LEFT_ARROW) {
     s.x -=50;
+    
   }
   if(keyCode == RIGHT_ARROW) {
     s.x +=50;
+     
   }
 }
+
 
 function gridDraw() {
 	for(let i=35; i<=500; i+=50) {
@@ -52,5 +85,3 @@ function gridDraw() {
 		}
 	}
 }
-
-
